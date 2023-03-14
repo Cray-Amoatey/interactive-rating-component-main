98e26cc359db16755eba0b7c35a9f1f7ead38bb3
Cray-Amoatey <amoatey.cray@gmail.com>
2023-03-14 15:20:37 +0000

    progression - JS

--- main.js
+++ main.js
@@ -54,20 +54,44 @@ let ratingDisplay = document.getElementById('ratingDisplay')
 
 
 
+// const rating = document.querySelectorAll(".circle");
+// const textDisplay = document.getElementById("textDisplay");
+// const submitBtn = document.getElementById("submitBtn");
+// const appreciationCard = document.getElementById("appreciationCard");
+// const supportCard = document.getElementById("supportCard");
+
+// for (let i = 0; i < rating.length; i++) {
+//   rating[i].addEventListener("click", () => {
+//     textDisplay.innerHTML = `You selected ${rating[i].innerHTML} out of 5`;
+//   });
+
+//   submitBtn.addEventListener("click", () => {
+//     appreciationCard.style.display = "initial";
+//     supportCard.style.display = "none";
+//   });
+// }
+
+
+
 const rating = document.querySelectorAll(".circle");
 const textDisplay = document.getElementById("textDisplay");
 const submitBtn = document.getElementById("submitBtn");
 const appreciationCard = document.getElementById("appreciationCard");
 const supportCard = document.getElementById("supportCard");
 
+let selectedRating = null;
+
 for (let i = 0; i < rating.length; i++) {
   rating[i].addEventListener("click", () => {
-    // rating.style.backgroundColor = 'red';
-    textDisplay.innerHTML = `You selected ${rating[i].innerHTML} out of 5`;
-  });
-
-  submitBtn.addEventListener("click", () => {
-    appreciationCard.style.display = "initial";
-    supportCard.style.display = "none";
+    selectedRating = rating[i];
+    textDisplay.innerHTML = `You selected ${selectedRating.innerHTML} out of 5`;
+    selectedRating.style.backgroundColor = "red";
   });
 }
+
+submitBtn.addEventListener("click", () => {
+  if (selectedRating !== null) {
+    appreciationCard.style.display = "initial";
+    supportCard.style.display = "none";
+  }
+});
